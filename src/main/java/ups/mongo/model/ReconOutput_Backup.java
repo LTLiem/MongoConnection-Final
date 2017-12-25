ef8de3c3ffe3f173f5175c0f8e9d34a7717a0daf
LiemLe <lthanhliem38@gmail.com>
2017-12-25 11:16:32 +0700

    Update recon output

--- src/main/java/ups/mongo/model/ReconOutput_Backup.java
+++ src/main/java/ups/mongo/model/ReconOutput_Backup.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "ReconOutput")
-public class ReconOutput {
+public class ReconOutput_Backup {
 
 	@Id
 	private ObjectId id;
@@ -28,33 +28,42 @@ public class ReconOutput {
 	@Field(value = "recon_time")
 	private Date reconTime;
 
-	@Field(value = "keys")
-	private String key;
+	@Field(value = "column_difference")
+	private List<String> columnDiff;
 
+	@Field(value = "missing_mx2")
+	private List<String> missingMx2;
+
+	@Field(value = "missing_mx3")
+	private List<String> missingMx3;
+
+	@Field(value = "keys")
+	private List<String> keys;
+
+	@Field(value = "headers")
+	private List<String> headers;
+	
 	@Field(value = "rows")
 	private List<String> rows;
 
-	public ReconOutput() {
-		super();
+	public ReconOutput_Backup() {
+
 	}
 
-	public ReconOutput(String reportId, String reportName, String reportingDate, Date reconTime,
-			String key, List<String> rows) {
+	public ReconOutput_Backup(String reportId, String reportName, String reportingDate, Date reconTime,
+			List<String> columnDiff, List<String> missingMx2, List<String> missingMx3, List<String> keys,
+			List<String> headers, List<String> rows) {
 		super();
 		this.reportId = reportId;
 		this.reportName = reportName;
 		this.reportingDate = reportingDate;
 		this.reconTime = reconTime;
-		this.key = key;
+		this.columnDiff = columnDiff;
+		this.missingMx2 = missingMx2;
+		this.missingMx3 = missingMx3;
+		this.keys = keys;
+		this.headers = headers;
 		this.rows = rows;
-	}
-
-	public ObjectId getId() {
-		return id;
-	}
-
-	public void setId(ObjectId id) {
-		this.id = id;
 	}
 
 	public String getReportId() {
@@ -89,12 +98,44 @@ public class ReconOutput {
 		this.reconTime = reconTime;
 	}
 
-	public String getKey() {
-		return key;
+	public List<String> getColumnDiff() {
+		return columnDiff;
 	}
 
-	public void setKey(String key) {
-		this.key = key;
+	public void setColumnDiff(List<String> columnDiff) {
+		this.columnDiff = columnDiff;
+	}
+
+	public List<String> getMissingMx2() {
+		return missingMx2;
+	}
+
+	public void setMissingMx2(List<String> missingMx2) {
+		this.missingMx2 = missingMx2;
+	}
+
+	public List<String> getMissingMx3() {
+		return missingMx3;
+	}
+
+	public void setMissingMx3(List<String> missingMx3) {
+		this.missingMx3 = missingMx3;
+	}
+
+	public List<String> getKeys() {
+		return keys;
+	}
+
+	public void setKeys(List<String> keys) {
+		this.keys = keys;
+	}
+
+	public List<String> getHeaders() {
+		return headers;
+	}
+
+	public void setHeaders(List<String> headers) {
+		this.headers = headers;
 	}
 
 	public List<String> getRows() {
--- src/main/java/ups/mongodb/App.java
+++ src/main/java/ups/mongodb/App.java
@@ -1,10 +1,16 @@
 package ups.mongodb;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.stream.Collectors;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import ups.mongo.configuration.SpringMongoConfig;
 import ups.mongo.fileprocess.MongoDataUtil;
+import ups.mongo.model.ReconInputMx2;
 import ups.mongo.service.AutoTagService;
 import ups.mongo.service.ClusterOutputService;
 import ups.mongo.service.ReconInputServiceMx2;
@@ -14,23 +20,32 @@ import ups.mongo.service.impl.ClusterOutputServiceImpl;
 
 public class App {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) throws ParseException {		
 	
-		ClusterOutputService cService = MongoDataUtil.getClusterOutputService();				
-		System.out.println(cService.getByReportId("R330").get(0).getMergerRate());
+		//ClusterOutputService cService = MongoDataUtil.getClusterOutputService();				
+		//System.out.println(cService.getByReportId("R320").get(0).getMergerRate());
 		
 		
-		AutoTagService aService = MongoDataUtil.getAutoTagService();				
-		System.out.println(aService.getByReportId("R111"));
+		//AutoTagService aService = MongoDataUtil.getAutoTagService();				
+		//System.out.println(aService.getByReportId("R111"));
 		
 		ReconInputServiceMx2 rec2Service = MongoDataUtil.getReconInputServiceMx2();				
-		System.out.println(rec2Service.getByReportId("R111"));
+		System.out.println(rec2Service.getByReportId("R320").get(0).getImportingTime());
+		
+		/*String tdate = "Wed Nov 08 09:53:24 ICT 2017";
+		SimpleDateFormat sdf = new SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy");
+		Date pubDate = sdf.parse(tdate);
+		System.out.println(pubDate);*/
 		
 		ReconInputServiceMx3 rec3Service = MongoDataUtil.getReconInputServiceMx3();				
-		System.out.println(rec3Service.getByReportId("R320"));
+		System.out.println(rec3Service.getByReportId("R320").get(0).getImportingTime());
 		
-		ReconOutputService outService = MongoDataUtil.getReconOutputService();				
-		System.out.println(outService.getByReportId("FXD10"));
+		ReconInputMx2 mx2 = (ReconInputMx2) rec2Service.getByReportIdAndReportingDate("R320", "2017 Sep 20")
+				.stream().filter(t -> "Thu Nov 09 14:38:17 ICT 2017".equals(t.getImportingTime().toString().trim()))
+				.collect(Collectors.toList()).get(0);
+		System.out.println(mx2.getReportingDate());
+		//ReconOutputService outService = MongoDataUtil.getReconOutputService();				
+		//System.out.println(outService.getByReportId("FXD10"));
 		
 	}
 }
